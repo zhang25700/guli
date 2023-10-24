d3058435450999b39a425c00dc5437e738f3ad95
ZZY <2570058832@qq.com>
2023-10-24 20:09:29 +0800

    测试通过controller

---
+++ guli_parent/service/service_edu/src/main/java/com/guli/eduservice/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.guli.eduservice.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zzy
+ * @description: TODO
+ * @date 2023/10/20 19:30
+ */
+@Configuration
+@MapperScan(basePackages = {"com.guli.eduservice.mapper"})
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return  mybatisPlusInterceptor;
+    }
+}
